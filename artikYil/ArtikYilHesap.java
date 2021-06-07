cd103a9c3cd53f61fd5d3663d525231246b5b817
yaemtaUni <yaemta@gmail.com>
2021-06-07 13:18:53 +0300

    first commit

---
+++ artikYil/ArtikYilHesap.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ArtikYilHesap {
+
+    public static void main(String[] args) 
+    {
+        int year;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yıl Giriniz : ");
+        year = scanner.nextInt();
+
+
+        if (year%4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year %400 ==0)
+                {
+                    System.out.println(year + " bir artık yıldır !");
+                }
+                else
+                {
+                    System.out.println(year + " bir artık yıldır değildir !");
+                }                
+            }
+            else
+            {
+                System.out.println(year + " bir artık yıldır !");
+            }
+
+        }
+        else
+        {
+            System.out.println(year + " bir artık yıldır değildir !");
+        }
+
+    }
+
+    
+}
