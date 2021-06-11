24f203142f1c5458a0c234bcdddf78dd53482ad0
Yaemta <yaemta@gmail.com>
2021-06-11 20:06:24 +0300

    first commit

---
+++ harmonikOrtalama/HarmonikOrtalama.java
@@ -0,0 +1,12 @@
+public class HarmonikOrtalama {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += 1.0/numbers[i];
+        }
+
+        System.out.println(sum);
+    }
+    
+}
