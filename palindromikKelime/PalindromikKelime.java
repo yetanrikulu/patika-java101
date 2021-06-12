5f3e2171f8c3ca04f7984b1b5939d21d8c4d9a0b
Yaemta <yaemta@gmail.com>
2021-06-12 23:49:31 +0300

    first commit

---
+++ palindromikKelime/PalindromikKelime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromikKelime {
+
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        
+        String str;
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Kelimeyi giriniz : ");
+        str = scanner.nextLine();
+
+        System.out.println(isPalindrome(str));
+
+
+    }
+    
+}
