ce78345ed7293461c85ab922a5787ab4cad0f40b
Yaemta <yaemta@gmail.com>
2021-09-07 10:49:52 +0300

    first commit

---
+++ notOrtalamasi/NotOrtalamasi.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+
+    public static void main(String[] args) {
+
+        int mat, fizik,kimya,turkce,tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz : ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik = inp.nextInt();
+
+        int toplam = mat+fizik+kimya+turkce+tarih+muzik;
+        double sonuc = toplam/6;
+
+        System.out.println("Ortalamanız : " + sonuc);
+
+        String sinifGecmeDurumu = sonuc > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(sinifGecmeDurumu);
+
+    }
+}
